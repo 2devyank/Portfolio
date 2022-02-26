2297825a187e52a1f6176b65223ff28effe17b3c
Devyank Nagpal <78840243+2devyank@users.noreply.github.com>
2022-02-26 21:17:10 +0530

    Update First.jsx

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -8,28 +8,28 @@ export default function First() {
 <div className='big'>
   <div className='half'>
   <div className='s'>
-    <img src="https://img.icons8.com/ios/2x/java-coffee-cup-logo.png" alt="" style={{height:"40px",width:40px}}/>
-  <img src="https://img.icons8.com/color/2x/javascript.png" alt="" style={{height:"40px",width:40px}}/>
+    <img src="https://img.icons8.com/ios/2x/java-coffee-cup-logo.png" alt="" style={{height:"30px",width:30px}}/>
+  <img src="https://img.icons8.com/color/2x/javascript.png" alt="" style={{height:"30px",width:30px}}/>
   </div>
   <br />
-  <div><h3>
+//   <div><h3>
     
-    Languages
-    </h3>
-    </div>
+//     Languages
+//     </h3>
+//     </div>
   </div>
  <div className='sec'>
  <div className='st'>
-    <img src="https://img.icons8.com/ultraviolet/2x/react.png" alt="" style={{height:"40px",width:40px}}/>
-    <img src="https://img.icons8.com/color/2x/vue-js.png" alt="" style={{height:"40px",width:40px}}/>
+    <img src="https://img.icons8.com/ultraviolet/2x/react.png" alt="" style={{height:"30px",width:30px}}/>
+    <img src="https://img.icons8.com/color/2x/vue-js.png" alt="" style={{height:"30px",width:30px}}/>
   </div>
   <br />
-   <div>
-<h3>
+//    <div>
+// <h3>
   
-  FrameWorks
-  </h3>
-   </div>
+//   FrameWorks
+//   </h3>
+//    </div>
  </div>
   
   
